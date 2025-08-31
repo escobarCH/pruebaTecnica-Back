18df7d04f0047d154b118a0623e3f549389909d5
escobarCH <abczd41@gmall.com>
2025-08-31 14:41:54 -0500

    se hace back

---
+++ src/main/java/com/example/backpruebaTecnica/BackpruebaTecnicaApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.backpruebaTecnica;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class BackpruebaTecnicaApplicationTests {
+
+}
